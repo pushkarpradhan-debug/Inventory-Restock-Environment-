53c9bf9882b08aa4a89da91d350e5e5ef002fa3a
Pushkar Pradhan <pushkarpradhanpardikar@gmail.com>
2026-04-08 19:43:53 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -112,10 +112,5 @@ def main():
     uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
 
 
-def main():
-    import uvicorn
-    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
-
-
 if __name__ == "__main__":
     main()
